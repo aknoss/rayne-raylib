33ce1ea8f1489d710f7eea57241055c26356d2cc
aknoss <pedrorfig27@gmail.com>
2025-11-13 20:52:36 -0300

    chore: testing physics with some rectangles

--- src/game.c
+++ src/game.c
@@ -1,17 +1,56 @@
 #include "game.h"
 #include "raylib.h"
+#include <stdbool.h>
 
 static int finishScreen = 0;
+struct Player {
+  Rectangle collisionMesh;
+};
+
+struct Player player;
+Rectangle ground;
+bool isPlayerOnGround;
 
 void DrawLevel1(void) {
-  DrawRectangle(0, GetScreenHeight() - 40, GetScreenWidth(), 40, BROWN);
+  DrawRectangle(ground.x, ground.y, ground.width, ground.height, BROWN);
 }
 
-void InitGameScreen(void) { finishScreen = 0; }
+void InitGameScreen(void) {
+  finishScreen = 0;
+  isPlayerOnGround = false;
 
-void UpdateGameScreen(void) {}
+  player.collisionMesh.x = 100;
+  player.collisionMesh.y = 400;
+  player.collisionMesh.width = 40;
+  player.collisionMesh.height = 80;
+  ground.x = 0;
+  ground.y = GetScreenHeight() - 40;
+  ground.width = GetScreenWidth();
+  ground.height = 40;
+}
 
-void DrawGameScreen(void) { DrawLevel1(); }
+void UpdateGameScreen(void) {
+  if (CheckCollisionRecs(ground, player.collisionMesh)) {
+    isPlayerOnGround = true;
+  } else {
+    isPlayerOnGround = false;
+  }
+  if (IsKeyDown(KEY_RIGHT)) {
+    player.collisionMesh.x += 5;
+  }
+  if (IsKeyDown(KEY_LEFT)) {
+    player.collisionMesh.x -= 5;
+  }
+  if (!isPlayerOnGround) {
+    player.collisionMesh.y += 5;
+  }
+}
+
+void DrawGameScreen(void) {
+  DrawLevel1();
+  DrawRectangle(player.collisionMesh.x, player.collisionMesh.y,
+                player.collisionMesh.width, player.collisionMesh.height, RED);
+}
 
 void UnloadGameScreen(void) {
   // TODO: Unload GAMEPLAY screen variables here!
