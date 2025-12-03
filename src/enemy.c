076740aac6bfdde34474a0a855ce4b8b6c3b0e7e
aknoss <pedrorfig27@gmail.com>
2025-12-03 14:59:23 -0300

    feat: add enemy queue

---
+++ src/enemy.c
@@ -0,0 +1,53 @@
+#include "enemy.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+EnemyQueue *newQueue() {
+  EnemyQueue *q = (EnemyQueue *)malloc(sizeof(EnemyQueue));
+  q->front = q->rear = NULL;
+  q->size = 0;
+  return q;
+}
+
+EnemyNode *newNode(Enemy enemy) {
+  EnemyNode *node = (EnemyNode *)malloc(sizeof(EnemyNode));
+  node->enemy = enemy;
+  node->next = NULL;
+  return node;
+}
+
+bool isQueueEmpty(EnemyQueue *q) { return q->front == NULL; }
+
+void enqueue(EnemyQueue *q, Enemy enemy) {
+  EnemyNode *node = newNode(enemy);
+
+  if (isQueueEmpty(q)) {
+    q->front = q->rear = node;
+  } else {
+    q->rear->next = node;
+    q->rear = node;
+  }
+
+  q->size++;
+}
+
+Enemy dequeue(EnemyQueue *q) {
+  if (isQueueEmpty(q)) {
+    printf("Error: queue is empty. Cannot dequeue.\n");
+    Enemy errorEnemy;
+    return errorEnemy;
+  }
+
+  EnemyNode *dequeuedNode = q->front;
+  Enemy dequeuedEnemy = dequeuedNode->enemy;
+
+  q->front = q->front->next;
+  free(dequeuedNode);
+
+  if (q->front == NULL) {
+    q->rear = NULL;
+  }
+
+  q->size--;
+  return dequeuedEnemy;
+}
---
+++ src/enemy.h
@@ -0,0 +1,29 @@
+#include "raylib.h"
+
+#ifndef ENEMY_H
+#define ENEMY_H
+
+typedef struct {
+  Vector2 position;
+  Rectangle collider;
+  int velocity;
+} Enemy;
+
+typedef struct EnemyNode {
+  Enemy enemy;
+  struct EnemyNode *next;
+} EnemyNode;
+
+typedef struct EnemyQueue {
+  int size;
+  EnemyNode *front;
+  EnemyNode *rear;
+} EnemyQueue;
+
+EnemyQueue *newQueue();
+EnemyNode *newNode(Enemy);
+bool isQueueEmpty(EnemyQueue *);
+void enqueue(EnemyQueue *, Enemy);
+Enemy dequeue(EnemyQueue *);
+
+#endif // ENEMY.H
